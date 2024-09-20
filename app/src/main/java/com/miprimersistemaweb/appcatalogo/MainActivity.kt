8912742cbc468e1a8f410f23e55c6fbe0edac2d2
Lucila Cruz <luci.carmen.98@gmail.com>
2024-09-19 20:11:56 -0500

    inicializar variables

--- app/src/main/java/com/miprimersistemaweb/appcatalogo/MainActivity.kt
+++ app/src/main/java/com/miprimersistemaweb/appcatalogo/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.miprimersistemaweb.appcatalogo
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //inicializar variables
+    private lateinit var txtEmail:EditText
+    private lateinit var txtPasswordLogin:EditText
+    private lateinit var btnIniciarSesion: Button
+    private lateinit var btnRegistrarse:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    }
+
+    override fun onStart() {
+        super.onStart()
+        inicializarComponentes()
+        incializarEventos()
+
+    }
+    private fun inicializarComponentes(){
+        txtEmail = findViewById(R.id.txtEmailLogin)
+        txtPasswordLogin=findViewById(R.id.txtPasswordLogin)
+        btnIniciarSesion = findViewById(R.id.btnIngresar)
+        btnRegistrarse=findViewById(R.id.btnRegistrarLogin)
+    }
+    private fun incializarEventos(){
+        btnIniciarSesion.setOnClickListener {
+            Toast.makeText(this,"iniciar sesion",Toast.LENGTH_LONG).show()
+        }
+        btnRegistrarse.setOnClickListener {
+            Toast.makeText(this,"registrarse",Toast.LENGTH_LONG).show()
         }
     }
+
 }
